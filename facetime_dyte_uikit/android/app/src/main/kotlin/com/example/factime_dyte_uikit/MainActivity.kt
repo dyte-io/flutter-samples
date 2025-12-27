3c517398ee7c22a2a6349b281febe5fa61e884be
Aman Kumar <thisisamank@gmail.com>
2025-12-27 20:19:35 +0530

    feat: migrate facetime sample app
    commit-id:ac2a085e

--- facetime_dyte_uikit/android/app/src/main/kotlin/com/example/factime_dyte_uikit/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.factime_dyte_uikit
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ facetime_rtk_ui/android/app/src/main/kotlin/com/example/facetime_rtk_ui/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.facetime_rtk_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
